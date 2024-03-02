186a5e3628d57b01b96c1c83f047e9fc14278bef
JuniorDev1k <ayoubdev1k@gmail.com>
2024-03-02 15:13:42 +0100

    api-route-for-translate

--- app/page.js
+++ app/page.js
@@ -1,40 +1,7 @@
-"use client";
-import { collection, doc, getDoc, getDocs, docs } from "firebase/firestore";
-import { db } from "../firebase/config";
-import { useState, useEffect } from "react";
-import { AmpContext } from "next/dist/server/future/route-modules/app-page/vendored/contexts/entrypoints";
+import React from "react";
 
 const page = () => {
-  const [content, setContent] = useState();
-  useEffect(() => {
-    const fetchTranslations = async () => {
-      try {
-        const docRef = doc(db, "translations", "en");
-        const docSnap = await getDoc(docRef);
-        setContent(docSnap.data());
-        // console.log(content);
-      } catch (error) {
-        console.error("Error fetching translations:", error);
-      }
-    };
-
-    fetchTranslations();
-  });
-  return (
-    <div>
-      {content ? (
-        <>
-          <h1>This page is in english</h1>
-          <h1> {content.about} </h1>
-          <h1> {content.home} </h1>
-          <h3>{content.product}</h3>
-          <p>{content.desc}</p>
-          <p>{content.buy}</p>
-        </>
-      ) : (
-        <p>there s no data</p>
-      )}
-    </div>
-  );
+  return <div>page</div>;
 };
+
 export default page;
