53ac3cd7948abb740541f211c142bc6b5fa9c579
JuniorDev1k <ayoubdev1k@gmail.com>
2024-03-30 23:08:34 +0100

    Auth-Api-route for signup

---
+++ app/api/auth/signUp/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { admin, auth } from "@/firebase/firebaseAdmin";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export async function POST(req, res) {
+  if (!req.method == "POST") {
+    return NextResponse.json({ message: "Method Not Allowed" });
+  }
+
+  try {
+    const { email, password } = req.body;
+    console.log(email, password);
+    // const resp = await createUserWithEmailAndPassword(auth, email, password);
+    // console.log(resp);
+    // return NextResponse.json({
+    //   message: "User signed up successfully ",
+    // });
+  } catch (error) {
+    console.log("error mother fucker");
+    // console.log("Error creating user : ", error.message);
+    // return NextResponse.json({ message: "Server Error" });
+  }
+}
